59355b1a6f12d996f115f0a5f5530c84dcc87ee9
bootarou <bootarouapp@gmail.com>
2025-12-23 21:41:16 +0900

    Add SMD menu item to navigation - Add SMD menu item after accounts in navigation - Create SMD page configuration and basic view - Add SMD translations for en-us and ja

---
+++ src/config/pages/smd-list.js
@@ -0,0 +1,13 @@
+module.exports = {
+	path: '/smd',
+	name: 'smd-list',
+	meta: {
+		group: 'smd',
+		keepAliveGoTo: []
+	},
+	props: {
+		layout: 'flex',
+		storeNamespaces: [],
+		initActions: []
+	}
+};
